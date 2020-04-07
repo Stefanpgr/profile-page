952e4b50ae84185cfce8e8467fb00dd3ea983bd1
stefanpgr <stefanpongrz@gmail>
2020-04-07 05:42:39 -0700

    first commit

---
+++ src/Left.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Left = () => {
+  return (
+    <div class="left">
+      <img src="https://i.imgur.com/cMy8V5j.png" alt="user" width="100" />
+      <h4>Alex William</h4>
+      <p>UI Developer</p>
+    </div>
+  );
+};
+
+export default Left;
